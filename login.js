743f769e4cd7c1afb6baee1f836e91f602a0f9a6
Ironankit525 <ankitkumar17541@gmail.com>
2025-11-29 15:50:21 +0530

    main page update

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
-function handleLogin(event) {
-            event.preventDefault();
-            const collegeCode = document.getElementById('college-code').value;
-            console.log('Login attempt with code:', collegeCode);
-            alert('Login successful! Welcome to College Portal.');
-            // Add your actual login logic here
-        }
+// function handleLogin(event) {
+//             event.preventDefault();
+//             const collegeCode = document.getElementById('college-code').value;
+//             console.log('Login attempt with code:', collegeCode);
+//             alert('Login successful! Welcome to College Portal.');
+//             // Add your actual login logic here
+//         }
